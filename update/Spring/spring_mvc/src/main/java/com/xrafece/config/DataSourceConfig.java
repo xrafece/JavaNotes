7eae4f51f84ee665829b6cf7922d0c85bf64412f
Xrafece <286402807@qq.com>
2020-07-30 23:59:51 +0800

    init spring_mvc with anno config

--- update/Spring/spring_mvc/src/main/java/com/xrafece/config/DataSourceConfig.java
+++ update/Spring/spring_mvc/src/main/java/com/xrafece/config/DataSourceConfig.java
@@ -1,10 +1,32 @@
 package com.xrafece.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
 
 /**
  * @author Xrafece
  */
 @PropertySource("classpath:jdbc.properties")
 public class DataSourceConfig {
+    @Value("${jdbc.driver}")
+    private String driver;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+    @Bean
+    public DataSource druidDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
 }
