1d62cbac3a6f621bdd4c61769d880f2f780daaec
Xrafece <286402807@qq.com>
2020-07-30 23:58:47 +0800

    init spring_mvc with anno confid

---
+++ update/Spring/spring_mvc/src/main/java/com/xrafece/config/DataSourceConfig.java
@@ -0,0 +1,10 @@
+package com.xrafece.config;
+
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @author Xrafece
+ */
+@PropertySource("classpath:jdbc.properties")
+public class DataSourceConfig {
+}
--- update/Spring/spring_mvc/src/main/java/com/xrafece/config/SpringConfig.java
+++ update/Spring/spring_mvc/src/main/java/com/xrafece/config/SpringConfig.java
@@ -2,11 +2,13 @@ package com.xrafece.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author Xrafece
  */
 @Configuration
 @ComponentScan("com.xrafece")
+@Import({DataSourceConfig.class, WebConfig.class})
 public class SpringConfig {
 }
