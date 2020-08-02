fe2e30aa157cdf97947e7b9a7be4709f5f8b631d
Xrafece <286402807@qq.com>
2020-08-02 12:43:01 +0800

    add springboot quick_start and modify spring_mvc spring_web agagin

--- update/SpringBoot/quick_start/src/main/java/com/xrafece/controller/HelloController.java
+++ update/SpringBoot/quick_start/src/main/java/com/xrafece/controller/HelloController.java
@@ -1,7 +1,16 @@
 package com.xrafece.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Xrafece
  */
+@RestController
 public class HelloController {
+    @RequestMapping("hello")
+    public String HelloWorld() {
+        return "Hello World";
+    }
 }
