f63e9a04f1c408538d902c2af971b63d79f28422
Xrafece <286402807@qq.com>
2020-08-05 18:35:36 +0800

    updaterefresh SpringBoot project file structure, add hello and confuguration

---
+++ update/SpringBoot/configuration/src/main/java/com/xrafece/ConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.xrafece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationApplication.class, args);
+	}
+
+}
