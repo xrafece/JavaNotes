8ce419661f6682c5cbce11028d7dc27afee0e1f3
Xrafece <286402807@qq.com>
2020-08-19 00:27:21 +0800

    update SpringBoot.bak Initialize jjwt project

---
+++ update/SpringBoot.bak/jjwt/src/main/java/com/xrafece/JjwtApplication.java
@@ -0,0 +1,13 @@
+package com.xrafece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjwtApplication.class, args);
+	}
+
+}
